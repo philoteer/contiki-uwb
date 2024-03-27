d378ad69e05e39c2417118a59440f8a6e59c3e28
philoteer <ksshin@postech.ac.kr>
2024-03-28 02:20:13 +0900

    Update dwm1001_uart_reader.py

--- examples/ranging/dwm1001_uart_reader.py
+++ examples/ranging/dwm1001_uart_reader.py
@@ -4,17 +4,34 @@
 import serial
 import threading
 import time
+import pylink
 
 # Configurations
 #################################################################
 PORT_NAMES = {"one":"/dev/ttyACM0","two":"/dev/ttyACM1"}
 SAVE_PATHS = {"one":"out1.txt","two":"out2.txt"}
 INDICATOR = {"one":"*","two":"#"}
+JLINK_DEV_LIST = [760126688, 760126516] #JLinkExe -> showemulist
 
 ser = {}
 f_out = {}
 num_buf = {}
 header_cnt = {}
+
+GPIO_DEVICE = "/dev/ttyUSB0"
+GPIO_UP_COMMAND=bytes("pin.value(1) \r\n", "ascii")
+GPIO_DOWN_COMMAND=bytes("pin.value(0) \r\n", "ascii")
+
+ser_GPIO_DEVICE = serial.Serial(
+	port = GPIO_DEVICE, 
+	baudrate=115200, 
+	parity='N',
+	stopbits=1,
+	bytesize=8,
+	timeout=8,
+	xonxoff=0
+)
+
 #################################################################
 
 # Functions
@@ -35,21 +52,33 @@ def rx_thread(PORT_NAME):
 			
 #Implement this to send the start signal (via GPIO or something)
 def gpio_signal_start():
-	print("Start signal (not implemented)")
-	pass
+	print("Start signal")
+	ser_GPIO_DEVICE.write(GPIO_UP_COMMAND)
+	print(ser_GPIO_DEVICE.readline().decode("ASCII"))
 	
 #Implement this to send the end signal (via GPIO or something)
 def gpio_signal_end():
-	print("End signal (not implemented)")
-	pass
+	print("End signal")
+	ser_GPIO_DEVICE.write(GPIO_DOWN_COMMAND)
+	print(ser_GPIO_DEVICE.readline().decode("ASCII"))
+
 #################################################################
 
 # main()
 #################################################################
+
 gpio_signal_end()
 print("press enter to continue.")
 input()
 
+for DEV in JLINK_DEV_LIST:
+	jlink = pylink.JLink()
+	jlink.open(DEV)
+	jlink.connect("nrf52832_xxAA",speed=1000)
+	print(f"Jlink status: {jlink.target_connected()}")
+	jlink.reset()
+	jlink.close()
+	
 start_time = time.time()
 gpio_signal_start()
 
@@ -89,4 +118,6 @@ while (True):
 		gpio_signal_end()
 		break
 
+print("done.")
+input()
 ################################################################
